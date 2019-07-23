e473362f2a9db51683327885d24a3c41e6b91cd2
pietrodito <pietrodito@gmail.com>
2019-07-23 19:11:24 +0200

    oh-my-zsh theme added

--- installers/oh-my-zsh.sh
+++ installers/oh-my-zsh.sh
@@ -5,3 +5,6 @@ sudo chsh -s /bin/zsh $USER
 wget  https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P ~
 zsh ~/install.sh
 rm ~/install.sh
+
+cp helpers/ulys.zsh-theme ~/.oh-my-zsh/themes
+sed -i 's/robbyrussell/ulys/' ~/.zshrc
