651a491cdf29ab27886511923a00cb5affd01d19
pietrodito <pietrodito@gmail.com>
2019-07-23 14:02:38 +0200

    Update aliases.zsh

--- helpers/aliases.zsh
+++ helpers/aliases.zsh
@@ -1,8 +1,4 @@
 alias cd..='cd ..'
 alias chx='chmod +x'
-alias rnow='sudo shutdown -r now'
-alias snow='sudo shutdown -h now'
 alias md='mkdir'
 alias rmr='rm -r'
-alias monitom='terminator -l TOMCAT'
-
