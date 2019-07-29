54c2cf837a858b718e58e751a18004ef56981daf
pietrodito <pietrodito@gmail.com>
2019-07-29 12:44:58 +0200

    Update run-installers.sh

--- run-installers.sh
+++ run-installers.sh
@@ -1,6 +1,8 @@
 ./installers/top-setup.sh
 ./installers/vim-setup.sh
 ./installers/oh-my-zsh.sh
-./installers/git-setup.sh
 ./installers/aliases-setup.sh
+./installers/git-setup.sh
 ./installers/x11-display-config.sh
+
+rm ~/.bash*
