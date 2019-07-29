90173c18fe8c1a8edbf58eaae58b8bef628f66c8
pietrodito <pietrodito@gmail.com>
2019-07-29 16:22:23 +0200

    X11 setup simplified using /etc/resolv.conf

--- helpers/source-me-to-configure-x11-with-wsl2.sh
+++ helpers/source-me-to-configure-x11-with-wsl2.sh
@@ -1,10 +1,3 @@
-ipconfig.exe > ~/.ipconfig-result &
-pid=$!
-sleep 1
-kill -9 $pid
-ip=$(cat ~/.ipconfig-result | grep -A 10 WSL | grep IPv4 | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-rm ~/.ipconfig-result
-
-export DISPLAY=$ip:0
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
 export LIBGL_ALWAYS_INDIRECT=1
 
