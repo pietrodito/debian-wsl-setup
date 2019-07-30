c19b5aee789c587ee465fc8f9f195891d2ba376b
pietrodito <pietrodito@gmail.com>
2019-07-30 15:14:06 +0200

    time config is done at the same time as X11 config

--- helpers/source-me-to-configure-x11-with-wsl2.sh
+++ helpers/source-me-to-configure-x11-with-wsl2.sh
@@ -1,3 +1,6 @@
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
 export LIBGL_ALWAYS_INDIRECT=1
 
+# Configure time :
+sudo date +%T -s "$(sudo hwclock --show | awk '{ print [ }' | sed -r 's/(.*)\..*/\1/')""' ] }')"
+
