dff1db1cd7031e9185bbab3476b4338449e65486
pietrodito <pietrodito@gmail.com>
2019-08-26 11:40:56 +0200

    Update git-setup.sh

--- installers/git-setup.sh
+++ installers/git-setup.sh
@@ -1,3 +1,3 @@
-ln -s ~/Comp/debian-wsl-setup/helpers/dot.gitconfig ~/.gitconfig
+ln -sf ~/Comp/debian-wsl-setup/helpers/dot.gitconfig ~/.gitconfig
 mkdir -p ~/bin
-ln -s ~/Comp/debian-wsl-setup/helpers/pushit ~/bin/pushit
+ln -sf ~/Comp/debian-wsl-setup/helpers/pushit ~/bin/pushit
