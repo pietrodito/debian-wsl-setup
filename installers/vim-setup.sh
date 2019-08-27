309a83af1b2136e53994868779043ea866dfd0e2
pietrodito <pietrodito@gmail.com>
2019-08-27 19:28:30 +0200

    Update vim-setup.sh

--- installers/vim-setup.sh
+++ installers/vim-setup.sh
@@ -9,5 +9,5 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # Copy user config
-sudo ln -sf ~/Comp/debian-wsl-setup/helpers/my_configs.vim ~/.vim_runtime/
+sudo ln -sf ~/Comp/wsl-debian-setup/helpers/my_configs.vim ~/.vim_runtime/
 
