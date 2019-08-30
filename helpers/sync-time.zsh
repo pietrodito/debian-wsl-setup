f55abec620ecb08859e6e3e0f53902aa3bb889e5
pietrodito <pietrodito@gmail.com>
2019-08-30 07:44:40 +0200

    sync-time as function

--- helpers/sync-time.zsh
+++ helpers/sync-time.zsh
@@ -1,1 +1,1 @@
-sudo date +%T -s "$(sudo hwclock --show | awk '{ print $2 }' | sed -r 's/(.*)\..*/\1/')"
+sync-time () {sudo date +%T -s "$(sudo hwclock --show | awk '{ print $2 }' | sed -r 's/(.*)\..*/\1/')"}
